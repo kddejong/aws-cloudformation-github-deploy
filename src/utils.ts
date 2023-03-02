1d1a76343e732b80bd9df5a3558297bf981d67b6
Kevin DeJong <kddejong@amazon.com>
2023-03-02 15:01:18 -0800

    Comma separated lists

--- src/utils.ts
+++ src/utils.ts
@@ -50,12 +50,20 @@ export function parseParameters(parameterOverrides: string): Parameter[] {
   }
 
   const parameters = new Map<string, string>()
-  parameterOverrides.split(',').forEach(parameter => {
-    const [key, value] = parameter.trim().split('=')
-    let param = parameters.get(key)
-    param = !param ? value : [param, value].join(',')
-    parameters.set(key, param)
-  })
+  parameterOverrides
+    .split(/,(?=(?:(?:[^"']*["|']){2})*[^"']*$)/g)
+    .forEach(parameter => {
+      const [key, value] = parameter.trim().split('=')
+      let param = parameters.get(key)
+      param = !param ? value : [param, value].join(',')
+      if (
+        (param.startsWith("'") && param.endsWith("'")) ||
+        (param.startsWith('"') && param.endsWith('"'))
+      ) {
+        param = param.substring(1, param.length - 1)
+      }
+      parameters.set(key, param)
+    })
 
   return [...parameters.keys()].map(key => {
     return {
